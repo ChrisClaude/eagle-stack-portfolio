2c25fc6507b25bbcf4a1ae445dec03131db8c066
ChClaude <christ.tchambila@gmail.com>
2020-05-14 23:10:19 +0200

    inspire image size modified

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -5,6 +5,7 @@
         src="https://res.cloudinary.com/lulendo-technologies/image/upload/v1589488298/eagle-stack/vuetify-logo_n6o1du.svg"
         alt="Vuetify.js"
         class="mb-5"
+        style="width: 100px; height: 100px"
       >
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
